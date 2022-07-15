90c2b4cfbcb32bc953b4c0de5a3cefda74ffdab6
Danilo Ferreira <daniloferreiradasilva345@gmail.com>
2022-07-14 21:18:25 -0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,17 +5,9 @@ function Footer() {
     return (
         <div className="w-100 mx-auto">
             <footer className="py-4 my-4">
-                <ul className="nav justify-content-center border-bottom pb-3 mb-3">
-                    <li className="nav-item">
-                        <a href="https://www.accenture.com/" target="_blank" className="nav-link px-2"><img
-                            className="tamanho" src="/_uploads/accenture.png" /></a></li>
-                    <li className="nav-item">
-                        <a href="#" target="_blank" className="nav-link px-2 text-light">Sobre Nós:
-                            TecnoPower</a></li>
-                    <li className="nav-item"><a href="https://www.redecidada.org.br/" target="_blank" className="nav-link px-2"><img
-                        className="tamanho" src="/_uploads/redecidada.png" /></a></li>
-                </ul>
-                <p className="text-center text-light">&copy; 2022 TecnoPower</p>
+                <p className="text-center">
+                    <a href="#" target="_blank" className="nav-link px-2 text-light">Sobre Nós &copy; 2022 TecnoPower</a>
+                </p>
             </footer>
         </div>
     );
