227625bd41f5234de67c49eccff3372ab1b9252b
Danilo Ferreira <daniloferreiradasilva345@gmail.com>
2022-07-17 02:18:14 -0300

    Ajuste no layout do sobre

--- src/pages/Sobre.js
+++ src/pages/Sobre.js
@@ -12,11 +12,11 @@ export const Sobre =  ()=> {
 
         <div className="container mx-auto pt-5">
             <div className="card-group">
-                <div className="card">
+                <div className="card ps-3 pe-3">
                     <img src="uploads/danilo.png" className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">Danilo Ferreira</h5>
-                        <div className="card-footer bg-light">
+                        <div className="card-footer bg-white">
                             <div className="row">
                                 <div className="col">
                                     <a href="#" target="_blank"> <img src="uploads/linkedin.png" className="img-fluid"
@@ -34,11 +34,11 @@ export const Sobre =  ()=> {
                         </div>
                     </div>
                 </div>
-                <div className="card">
+                <div className="card ps-3 pe-3">
                     <img src="uploads/paulo.png" className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">Paulo Vinícius</h5>
-                        <div className="card-footer bg-light">
+                        <div className="card-footer bg-white">
                             <div className="row">
                                 <div className="col">
                                     <a href="#" target="_blank"> <img src="uploads/linkedin.png" className="img-fluid"
@@ -56,11 +56,11 @@ export const Sobre =  ()=> {
                         </div>
                     </div>
                 </div>
-                <div className="card">
+                <div className="card ps-3 pe-3">
                     <img src="uploads/mateus.png" className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">Mateus Moura</h5>
-                        <div className="card-footer bg-light">
+                        <div className="card-footer bg-white">
                             <div className="row">
                                 <div className="col">
                                     <a href="#" target="_blank"><img src="uploads/linkedin.png" className="img-fluid"
@@ -78,11 +78,11 @@ export const Sobre =  ()=> {
                         </div>
                     </div>
                 </div>
-                <div className="card">
+                <div className="card ps-3 pe-3">
                     <img src="uploads/ghabryellen.png" className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">Ghabryellen Aleska</h5>
-                        <div className="card-footer bg-light">
+                        <div className="card-footer bg-white">
                             <div className="row">
                                 <div className="col">
                                     <a href="#" target="_blank"> <img src="uploads/linkedin.png" className="img-fluid"
