792ca976a7728838cbf22d95d214edfae400549e
Danilo Ferreira <daniloferreiradasilva345@gmail.com>
2022-07-30 02:39:35 -0300

    Tela de Home Iniciada

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,67 +1,68 @@
 import { Navbar } from '../components/Navbar';
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { UserContext } from '../App';
 import { Navigate } from 'react-router';
+import { useNavigate } from 'react-router-dom';
+import { axiosInstance } from '../config/axios';
 export const Home = () => {
     const { token, setToken } = useContext(UserContext);
-    if (!token) {
-        return <Navigate to='/' replace={true} />;
-    }
-    if (localStorage.getItem('tipo')=='mentorado') {
+    const { tipo, setTipo } = useContext(UserContext);
+    const [mentor, setMentor] = useState([]);
+    let navigate = useNavigate();
+    useEffect(() => {
+        if (!token) {
+            navigate("/");
+        }
+    }, []);
+
+    useEffect(() => {
+        if (tipo === "mentorado") {
+            axiosInstance.get('/api/mentor').then((res) => {
+                const response = res;
+                setMentor(res.data)
+            });
+        }
+    }, []);
+function teste(botao){
+    console.log(botao)
+}
+    if (localStorage.getItem('tipo') == 'mentorado') {
         return (
             <>
                 <Navbar tipo={2} />
                 <div className="container mx-auto pt-3">
                     <div className="row row-cols-1 row-cols-md-2 g-4 pt-2">
-                        <div className="col">
-                            <div className="card">
-                                <div className="card-body">
-                                    <h5 className="card-title">Special title treatment</h5>
-                                    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                    <a className="btn btn-primary cursorPointer">Go somewhere</a>
+                        {mentor.map((row) => (
+                            <div className="col" key={row._id}>
+                                <div className="card text-center max-height">
+                                    <div className="card-header">
+                                        {row.area}
+                                    </div>
+                                    <div className="card-body">
+                                        <h5 className="card-title">{row.profissao}</h5>
+                                        <h3 className="card-title">{row.name}</h3>
+                                        <p className="card-text">{row.desc}</p>
+                                    </div>
+                                    <div className="card-footer">
+                                    <a className="btn btn-default cursorPointer w-80" value={row._id}
+                                    onClick={()=>teste(row._id)}>Conectar</a>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className="col">
-                            <div className="card">
-                                <div className="card-body">
-                                    <h5 className="card-title">Special title treatment</h5>
-                                    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                    <a className="btn btn-primary cursorPointer">Go somewhere</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col">
-                            <div className="card">
-                                <div className="card-body">
-                                    <h5 className="card-title">Special title treatment</h5>
-                                    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                    <a className="btn btn-primary cursorPointer">Go somewhere</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col">
-                            <div className="card">
-                                <div className="card-body">
-                                    <h5 className="card-title">Special title treatment</h5>
-                                    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                                    <a className="btn btn-primary cursorPointer">Go somewhere</a>
-                                </div>
-                            </div>
-                        </div>
+                        ))}
                     </div>
                 </div>
             </>
         )
     }
-    if (localStorage.getItem('tipo')=='mentor') {
+    if (localStorage.getItem('tipo') == 'mentor') {
         return (
             <>
-    
+
                 <Navbar tipo={2} />
-    
+
                 <div className="container mx-auto pt-3">
-                    
+
                     <table className="table">
                         <thead>
                             <tr>
@@ -95,11 +96,11 @@ export const Home = () => {
                 </div>
                 <div className="container">
                     <p className="text-center">
-                        <button className="btn btn-lg btn-primary mt-3" type="submit">Desvincular Conexão</button>
+                        <button className="btn btn-lg btn-default mt-3" type="submit">Desvincular Conexão</button>
                     </p>
                 </div>
             </>
         )
     }
-    
+
 }
