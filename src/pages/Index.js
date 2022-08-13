cbc1b416d0d66ff27da44ef56303a0dcc76198c3
Danilo Ferreira <daniloferreiradasilva345@gmail.com>
2022-08-13 17:24:41 -0300

    perf: startServer()

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -5,6 +5,7 @@ import { UserContext } from '../App';
 import { useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
 import { Container } from '../components/Styles/Styles';
+import { Loader } from '../components/Loader';
 import { ModalServidor, ModalMsgPreenchimento, ModalCadastros } from '../components/Modal/Modal';
 export const Index = ({ setMode }) => {
     const { setToken } = useContext(UserContext);
@@ -13,11 +14,18 @@ export const Index = ({ setMode }) => {
     const [modalShowModalServidor, setModalShowModalServidor] = useState(false);
     const [modalShowPreencher, setModalShowPreencher] = useState(false);
     const [modalCadastros, setModalCadastros] = useState(false);
+    const [ready, setReady] = useState(false);
     let navigate = useNavigate();
     useEffect(() => {
-        //Refresh para dar o start no deploy do heroku
-        axiosInstance.get('/').then((res) => console.log(res.data.msg))
-    },[])
+        async function startServer() {
+            await axiosInstance.get('/').then((res) => {
+                console.log(res.data.msg);
+                setReady(true)
+            })
+        }
+        startServer();
+    }, []);
+
     useEffect(() => {
         setMode("light");
     }, [setMode])
@@ -54,49 +62,55 @@ export const Index = ({ setMode }) => {
         }
     }
     return (
+        <>
+            {ready ?
+                <>
+                    <Container className="container form-login w-50">
+                        <form className='pt-5'>
+                            <div className="form-floating text-center" id='img-login'>
+                                <img className="mb-2 img-fluid" src="uploads/panic.gif" alt="" />
+                            </div>
+                            <h4 className="mb-3 mx-auto text-center">Plataforma de Apadrinhamento de Necessidades Íntegras e Comuns</h4>
 
-        <Container className="container form-login w-50">
-            <form className='pt-5'>
-                <div className="form-floating text-center" id='img-login'>
-                    <img className="mb-2 img-fluid" src="uploads/panic.gif" alt="" />
-                </div>
-                <h4 className="mb-3 mx-auto text-center">Plataforma de Apadrinhamento de Necessidades Íntegras e Comuns</h4>
+                            <div className="form-floating pb-3">
+                                <input value={login.email} onChange={(e) => { setLogin({ ...login, email: e.target.value }) }} required type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+                                <label htmlFor="floatingInput">Email</label>
+                            </div>
+                            <div className="form-floating">
+                                <input value={login.pass} onChange={(e) => { setLogin({ ...login, pass: e.target.value }) }} required type="password" className="form-control" id="senha-campo" placeholder="Password" autoComplete="off" />
+                                <label htmlFor="senha-campo">Senha</label>
+                            </div>
 
-                <div className="form-floating pb-3">
-                    <input value={login.email} onChange={(e) => { setLogin({ ...login, email: e.target.value }) }} required type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
-                    <label htmlFor="floatingInput">Email</label>
-                </div>
-                <div className="form-floating">
-                    <input value={login.pass} onChange={(e) => { setLogin({ ...login, pass: e.target.value }) }} required type="password" className="form-control" id="senha-campo" placeholder="Password" autoComplete="off" />
-                    <label htmlFor="senha-campo">Senha</label>
-                </div>
-
-                <div className="text-center pt-3">
-                    <button onClick={submitLogin}
-                        className="btn btn-default width-bt" type="submit">Entrar</button>
-                </div>
-            </form>
-            <div className="container">
-                <div className="text-center mt-2 pt-2">
-                    <a className="link-dark cursorPointer" href="/senha-no-log">Esqueci minha senha</a>
-                </div>
-            </div>
-            <div className="d-grid gap-2 mx-auto pb-5">
-                <ModalServidor
-                    show={modalShowModalServidor}
-                    onHide={() => setModalShowModalServidor(false)}
-                />
-                <ModalMsgPreenchimento
-                    show={modalShowPreencher}
-                    onHide={() => setModalShowPreencher(false)}
-                />
-                <ModalCadastros
-                    show={modalCadastros}
-                    onHide={() => setModalCadastros(false)}
-                />
-                <p variant="primary" onClick={() => setModalCadastros(true)} className="link-dark text-center mt-2 cursorPointer text-decoration-underline">Não possui uma conta? Cadastre-se</p>
-            </div>
-        </Container>
+                            <div className="text-center pt-3">
+                                <button onClick={submitLogin}
+                                    className="btn btn-default width-bt" type="submit">Entrar</button>
+                            </div>
+                        </form>
+                        <div className="container">
+                            <div className="text-center mt-2 pt-2">
+                                <a className="link-dark cursorPointer" href="/senha-no-log">Esqueci minha senha</a>
+                            </div>
+                        </div>
+                        <div className="d-grid gap-2 mx-auto pb-5">
+                            <ModalServidor
+                                show={modalShowModalServidor}
+                                onHide={() => setModalShowModalServidor(false)}
+                            />
+                            <ModalMsgPreenchimento
+                                show={modalShowPreencher}
+                                onHide={() => setModalShowPreencher(false)}
+                            />
+                            <ModalCadastros
+                                show={modalCadastros}
+                                onHide={() => setModalCadastros(false)}
+                            />
+                            <p variant="primary" onClick={() => setModalCadastros(true)} className="link-dark text-center mt-2 cursorPointer text-decoration-underline">Não possui uma conta? Cadastre-se</p>
+                        </div>
+                    </Container>
+                </>
+                : <Loader />
+            }
+        </>
     )
 
 }
